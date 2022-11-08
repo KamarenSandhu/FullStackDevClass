6b1c98cc20b13eea52579776e042f00d6fd6e1fd
Kamaren S <kamarensandhu56@gmail.com>
2022-11-07 18:36:05 -0600

    initial commit

---
+++ template.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3000
+
+const app = express();
+
+app.get('/', function(req,res){
+    res.send("Hello");
+})
+
+app.listen(port, function(){
+    console.log("Server is running on port " + port);
+})
