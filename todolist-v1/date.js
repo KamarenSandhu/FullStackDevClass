0be6dc3bdf773df18403079bd259166d1df8ce35
Kamaren S <kamarensandhu56@gmail.com>
2022-11-09 18:39:35 -0600

    Complete EJS Section

---
+++ todolist-v1/date.js
@@ -0,0 +1,30 @@
+
+
+exports.getDate = function() {
+    const today = new Date();
+    
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    
+    return today.toLocaleDateString('en-us',options);
+
+    return day;
+}
+
+exports.getDay = function (){
+    const today = new Date();
+    
+    const options = {
+        weekday: 'long'
+    };
+    
+    return today.toLocaleDateString('en-us',options);
+
+    return day;
+}
+
+console.log(module.exports)
+    
