b328eaa5605d1517c55d07582d21913cdfc4dbec
Kamaren S <kamarensandhu56@gmail.com>
2022-11-21 18:30:18 -0600

    part 2 commit

--- Keeper React App/src/components/App.jsx
+++ Keeper React App/src/components/App.jsx
@@ -2,12 +2,33 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes"
+
+// function createNote(entry){
+//     return(
+//         <Note 
+//             key = {entry.id}
+//             title = {entry.title}
+//             content = {entry.content}
+//         />
+//     )
+// }
+// alternative way of writing it
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            {/* alternative call of function */}
+            {/* notes.map(createNote) */}
+            {notes.map(entry => (
+            <Note 
+                key = {entry.id}
+                title = {entry.title}
+                content = {entry.content}
+            />
+            )
+            )}
             <Footer />
         </div>
     );
